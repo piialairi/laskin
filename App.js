4eb948c4acc2de92377cbbba2fc87ad1f33368a9
piialairi <piia.lairi@myy.haaga-helia.fi>
2023-08-29 18:23:45 +0300

    tehtava valmis

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [number, setNumber] = useState("");
+  const [secNumber, setSecNumber] = useState("");
+  let [result, setResult] = useState('')
+
+  const sumNumbers = () => {
+    result = Number(number) + Number(secNumber);
+    setResult(result);
+  }
+
+  const subtractNumbers = () => {
+    result = Number(number) - Number(secNumber);
+    setResult(result);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Result: {result}</Text>
+        <TextInput
+          keyboardType='numeric'
+          style={{ width: 200, borderColor: 'grey', borderWidth: 1 }}
+          onChangeText={number => setNumber(number)}
+        />
+        <TextInput
+          keyboardType='numeric'
+          style={{ width: 200, borderColor: 'grey', borderWidth: 1 }}
+          onChangeText={secNumber => setSecNumber(secNumber)}
+        />
+      </View>
+      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around' }}>
+        <Button onPress={sumNumbers} title='+' color={'black'} />
+        <Button onPress={subtractNumbers} title='-' />
+      </View>
     </View>
   );
 }
@@ -18,3 +47,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+//const [operation, setOperation] = useState(""); // add a state variable for the operation
+
+// <Text>Result: {operation === "+" ? Result() : operation === "-" ? Subtraction() : null} {number}</Text> 
+// use a ternary expression to render the result based on the operation
+
+// <Button onPress={() => setOperation("+")} title='+' color={'black'} /> 
+// use an arrow function to set the operation state to "+"
+// <Button onPress={() => setOperation("-")} title='-' /> 
+// use an arrow function to set the operation state to "-"
+
+//Tämä oli oma alkuperäinen ei-toimiva ratkaisu:
+/*<Text>Result: {Result()} {Subtraction()}</Text>
+  --
+  <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around' }}>
+        <Button onPress={Result} title='+' color={'black'} />
+        <Button onPress={Subtraction} title='-' />
+      </View >*/
